170e05dcd6b569c84fb0651a2c71fecfc1f5128a
VAntipenko <VAntipenko.02@gmail.com>
2021-05-03 23:48:57 +0300

    init

--- Js/6.js
+++ Js/6.js
@@ -1,14 +1,37 @@
-// Написать функцию для выполнения параллельных вычислений без использования Promise.
-var a = function (one, two) {
+const a = function (one, two) {
   return one + two;
 };
 
-var b = function () {
+const b = function () {
   return false;
 };
 
-const paralell = (a, b) => {};
+const call_function_async = (fn, args, done) => {
+  setTimeout(() => {
+    done(fn(...args));
+  }, 0);
+};
+
+const paralell = (calls, done) => {
+  let results = [];
+  let callsCount = 0;
+
+  if (!calls.length) {
+    done([]);
+  }
+
+  calls.forEach(([fn, args = []], index) =>
+    call_function_async(fn, args, (result) => {
+      results[index] = result;
+      callsCount += 1;
+
+      if (callsCount === calls.length) {
+        done(results);
+      }
+    })
+  );
+};
 
 paralell([[a, [1, 2]], [b]], function (results) {
-  console.log(results); // [3, false]
+  console.log(results);
 });
